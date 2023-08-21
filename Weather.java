d256e111a028af9278eabc81e895b588626f7640
Melissa Vega Acosta <70449208+melissavegaa@users.noreply.github.com>
2023-08-21 14:03:08 -0700

    Add files via upload

---
+++ Weather.java
@@ -0,0 +1,6 @@
+package com.grossmont.ws;
+
+public class Weather {
+    Weather_main main;
+    String name;
+}
---
+++ Weather_main.java
@@ -0,0 +1,5 @@
+package com.grossmont.ws;
+
+public class Weather_main {
+    float temp, pressure, humidity, temp_min, temp_max;
+}
